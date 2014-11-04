c0d79cc4f6f6e8c553cf1a3d75547964cec1cab8
dpop420 <dpop420@gmail.com>
2014-11-04 19:07:51 +0900

    fixed using webcam

---
+++ Assets/RotateBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var rotateFlag = 0;
+
+		if(rotateFlag == 1){
+			//transform.rotation = Quaternion.Euler(45, 45, 0);
+			//start animation
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
